0b53f38aa960c408327b36b1bd7261c967559f6b
Israel Alves <108505958+Khaldewey@users.noreply.github.com>
2024-03-08 17:20:49 +0000

    inserção de api do relógio universal

--- app/assets/javascripts/membro_cliente/functions.js
+++ app/assets/javascripts/membro_cliente/functions.js
@@ -1,49 +1,78 @@
 $(document).ready(function(){
-    $('.banner-carousel').lightSlider({
-        item:1,
-        slideMargin: 0,
-        loop: true,
-        auto: true,
-        speed: 1900,
-        pause: 7000,
-        pager: true
-    });
-    
-    
-    
-    var copyButton = document.getElementById('copy-link');
-    var iframe = document.querySelector('iframe');
+  $('.banner-carousel').lightSlider({
+      item:1,
+      slideMargin: 0,
+      loop: true,
+      auto: true,
+      speed: 1900,
+      pause: 7000,
+      pager: true
+  });
   
-    copyButton.addEventListener('click', function() {
-      var iframeHtml = iframe.outerHTML;
   
-      var input = document.createElement('input');
-      input.value = iframeHtml;
-      document.body.appendChild(input);
-      input.select();
-      document.execCommand('copy');
-      document.body.removeChild(input);
   
-      alert('Conteúdo do iframe copiado para a área de transferência.');
-    });
-    
-    const tipoDropdown = document.getElementById('tipo-dropdown');
-    const youtubeField = document.getElementById('youtube-field');
-    const vimeoField = document.getElementById('vimeo-field');
-    const imagemField = document.getElementById('imagem-field');
+  var copyButton = document.getElementById('copy-link');
+  var iframe = document.querySelector('iframe');
 
-    tipoDropdown.addEventListener('change', function() {
-      const selectedOption = tipoDropdown.value;
-      youtubeField.style.display = 'none';
-      vimeoField.style.display = 'none';
-      imagemField.style.display = 'none';
+  copyButton.addEventListener('click', function() {
+    var iframeHtml = iframe.outerHTML;
 
-      if (selectedOption === 'YouTube') {
-        youtubeField.style.display = 'block';
-      } else if (selectedOption === 'Vimeo') {
-        vimeoField.style.display = 'block';
-      } else if (selectedOption === 'Imagem') {
-        imagemField.style.display = 'block';
-      }
-    });
+    var input = document.createElement('input');
+    input.value = iframeHtml;
+    document.body.appendChild(input);
+    input.select();
+    document.execCommand('copy');
+    document.body.removeChild(input);
+
+    alert('Conteúdo do iframe copiado para a área de transferência.');
+  });
+  
+  const tipoDropdown = document.getElementById('tipo-dropdown');
+  const youtubeField = document.getElementById('youtube-field');
+  const vimeoField = document.getElementById('vimeo-field');
+  const imagemField = document.getElementById('imagem-field');
+
+  tipoDropdown.addEventListener('change', function() {
+    const selectedOption = tipoDropdown.value;
+    youtubeField.style.display = 'none';
+    vimeoField.style.display = 'none';
+    imagemField.style.display = 'none';
+
+    if (selectedOption === 'YouTube') {
+      youtubeField.style.display = 'block';
+    } else if (selectedOption === 'Vimeo') {
+      vimeoField.style.display = 'block';
+    } else if (selectedOption === 'Imagem') {
+      imagemField.style.display = 'block';
+    }
+  }); 
+  async function obterHorarioBrasilia() {
+    try {
+        const resposta = await fetch('http://worldtimeapi.org/api/timezone/America/Sao_Paulo');
+        const dados = await resposta.json();
+
+        // A propriedade "datetime" contém a data e hora atual em Brasília
+        const horarioBrasilia = new Date(dados.utc_datetime);
+        return horarioBrasilia;
+    } catch (erro) {
+        console.error('Erro ao obter o horário de Brasília:', erro);
+    }
+  }
+
+
+    // Função para atualizar o relógio
+    async function atualizarRelogioBrasilia() {
+      const dataAtual = await obterHorarioBrasilia();
+      const relogioElement = document.querySelector('h2');
+      const horaFormatada = dataAtual.toLocaleTimeString('pt-BR', { hour: '2-digit', minute: '2-digit' });
+      // Atualizar o conteúdo do elemento com o horário de Brasília
+      relogioElement.innerText = `Horário Oficial de Brasília: ${horaFormatada}`;
+    }
+
+    // Atualizar a cada minuto (60000 milissegundos)
+    setInterval(atualizarRelogioBrasilia, 60000);
+
+    // Chamar a função pela primeira vez
+    atualizarRelogioBrasilia();
+ 
 }); 
--- app/controllers/public_videos_controller.rb
+++ app/controllers/public_videos_controller.rb
@@ -1,13 +1,16 @@
 class PublicVideosController < ApplicationController
+  require 'tzinfo'
+  require 'net/http'
+  require 'json'
   def show  
-    require 'tzinfo'
+    
     customer_id = params[:customer_id]
     @customer = Customer.where(id: customer_id)
     @url = UrlStream.where(customer_id: @customer.ids).first
-    @blockages = Blockage.where(customer_id: customer_id, status: true)
-               
-    tz = TZInfo::Timezone.get('America/Sao_Paulo')
-    @data_atual_br = Time.now.in_time_zone(tz).strftime('%Y-%m-%d %H:%M:%S')
+    @data_atual_br = obter_horario_brasilia()
+    @blockages = Blockage.where(customer_id: customer_id, status: true).where('data_hora_inicio > ?', @data_atual_br)
+    #  -raise          
+    
     Rails.logger.info("O valor de data atual é: #{@data_atual_br}")
     
     if @blockages.present?
@@ -36,6 +39,30 @@ class PublicVideosController < ApplicationController
     else 
       @transmissao_bloqueada = false
       @iframe_src = @url.url
+    end
+  end 
+
+
+  private
+
+  def obter_horario_brasilia
+    begin
+      uri = URI('http://worldtimeapi.org/api/timezone/America/Sao_Paulo')
+      resposta = Net::HTTP.get_response(uri)
+  
+      if resposta.code == '200'
+        dados = JSON.parse(resposta.body)
+        horario_brasilia = Time.parse(dados['utc_datetime']).in_time_zone('America/Sao_Paulo')
+        formated_horario = horario_brasilia.strftime('%Y-%m-%d %H:%M:%S')
+        Rails.logger.info("Horário de Brasília obtido com sucesso: #{formated_horario}")
+        return formated_horario
+      else
+        Rails.logger.error("Erro ao obter o horário de Brasília. Código de resposta: #{resposta.code}")
+        raise StandardError, "Erro ao obter o horário de Brasília. Código de resposta: #{resposta.code}"
+      end
+    rescue StandardError => e
+      Rails.logger.error("Erro ao obter o horário de Brasília: #{e.message}")
+      raise e
     end
   end
 
